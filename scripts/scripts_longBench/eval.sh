4eda47b198457c857cc044255b50bff947807740
商昊暘521021910816 <assassin808@sjtu.edu.cn>
2025-01-10 15:47:07 +0800

    make script runable

--- scripts/scripts_longBench/eval.sh
+++ scripts/scripts_longBench/eval.sh
@@ -1,8 +1,8 @@
-export CUDA_VISIBLE_DEVICES=$1
+export CUDA_VISIBLE_DEVICES=0
 
-method=$2 # Support PyramidKV, SnapKV, H2O, StreamingLLM, CAM, L2Norm, ThinK
-max_capacity_prompts=$3 # 128,2048 in paper
-attn_implementation=$4 # Support "flash_attention_2", "sdpa", "eager".
+method=H2O # Support PyramidKV, SnapKV, H2O, StreamingLLM, CAM, L2Norm, ThinK
+max_capacity_prompts=64 # 128,2048 in paper
+attn_implementation=flash_attention_2 # Support "flash_attention_2", "sdpa", "eager".
 source_path=$5
 model_path=$6
 merge_method=$7 # Support "pivot"(LOOK-M_PivotMerge).
