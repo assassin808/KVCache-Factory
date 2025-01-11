4db75bfc59e2bee0acdef6028ab167fbfa69a0d2
assassin808 <assassin808@sjtu.edu.cn>
2025-01-11 15:30:38 +0800

    make script runable

--- scripts/scripts_longBench/eval.sh
+++ scripts/scripts_longBench/eval.sh
@@ -1,13 +1,13 @@
-export CUDA_VISIBLE_DEVICES=$1
+# export CUDA_VISIBLE_DEVICES=$1
 
-method=$2 # Support PyramidKV, SnapKV, H2O, StreamingLLM, CAM, L2Norm, ThinK
-max_capacity_prompts=$3 # 128,2048 in paper
-attn_implementation=$4 # Support "flash_attention_2", "sdpa", "eager".
-source_path=$5
-model_path=$6
-merge_method=$7 # Support "pivot"(LOOK-M_PivotMerge).
-quant_method=$7 # Support kivi and kvquant, default None.
-nbits=$8 # Quantization bit-width support 8,4,2. Need to set quant_method first.
+method="H2O" # Support PyramidKV, SnapKV, H2O, StreamingLLM, CAM, L2Norm, ThinK
+max_capacity_prompts=1 # 128,2048 in paper
+attn_implementation="flash_attention_2" # Support "flash_attention_2", "sdpa", "eager".
+source_path="./"
+model_path="/root/autodl-tmp/models--meta-llama--Llama-2-7b-chat-hf/snapshots/f5db02db724555f92da89c216ac04704f23d4590"
+# merge_method=$7 # Support "pivot"(LOOK-M_PivotMerge).
+# quant_method=$7 # Support kivi and kvquant, default None.
+# nbits=$8 # Quantization bit-width support 8,4,2. Need to set quant_method first.
 save_dir=${source_path}"results_long_bench" # path to result save_dir
 
 python3 run_longbench.py \
@@ -16,6 +16,6 @@ python3 run_longbench.py \
     --max_capacity_prompts ${max_capacity_prompts} \
     --attn_implementation ${attn_implementation} \
     --save_dir ${save_dir} \
-    --merge ${merge_method} \
-    --nbits ${nbits} \
-    --quant_method ${quant_method}
+    # --merge ${merge_method} \
+    # --nbits ${nbits} \
+    # --quant_method ${quant_method}
