5aa6f310852d012bb558fae95c006169ab3ec0a7
assassin808 <assassin808@sjtu.edu.cn>
2025-01-18 19:18:21 +0800

    k -> num_segments

--- pyramidkv/cache_utils_minicache.py
+++ pyramidkv/cache_utils_minicache.py
@@ -326,7 +326,7 @@ class DynamicCache(Cache):
         replaced_segment = set()
         for item in layer_map:
             i, j, seg, _ = item
-            if len(replaced_segment)>=k * 8:
+            if len(replaced_segment)>=num_segments * 8:
                 break
             if (j,seg) in used_segment or (j,seg) in replaced_segment or (i,seg) in used_segment:
                 continue
