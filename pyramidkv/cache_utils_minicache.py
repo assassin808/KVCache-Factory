81c271c87b6553295c747c285dc1fa233be03b4a
assassin808 <assassin808@sjtu.edu.cn>
2025-01-18 19:31:57 +0800

    using key similarity

--- pyramidkv/cache_utils_minicache.py
+++ pyramidkv/cache_utils_minicache.py
@@ -306,7 +306,7 @@ class DynamicCache(Cache):
                     for seg in range(num_segments):  # Only compare segments at the same position
                         k_prev_segment = self.retained_key_cache[i][:, :, seg*segment_size:(seg+1)*segment_size, :]
                         k_segment = self.retained_key_cache[j][:, :, seg*segment_size:(seg+1)*segment_size, :]
-                        k_similarity = torch.einsum("bhsd,bhsd->bhs", k_prev_segment, k_segment).mean().item()
+                        k_similarity = torch.einsum("bhsd,bhsd->bhs", k_prev_segment/k_prev_segment.norm(dim=-1,keepdim=True), k_segment/k_segment.norm(dim=-1,keepdim=True)).mean().item()
                         layer_map.append((i, j, seg, k_similarity))  # Store layer indices, segment index, and similarity
         layer_map.sort(key=lambda x:x[-1])
 
