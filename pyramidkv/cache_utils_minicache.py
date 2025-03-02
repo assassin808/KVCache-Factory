67a687d43786ab5db08b5f676e68ee073384b889
商昊暘521021910816 <assassin808@sjtu.edu.cn>
2025-03-02 23:58:49 +0800

    -n

--- pyramidkv/cache_utils_minicache.py
+++ pyramidkv/cache_utils_minicache.py
@@ -627,6 +627,7 @@ class DynamicCache(Cache):
             print('complete')
             # delete all temporary variables only keep the final key and value caches
             del temp_key, retained_keys, retained_values, layer_indices_full, layer_indices_compress, combined_range, all_indices, index_expanded, compress_index_expanded, selected_keys, selected_values, unselected_keys, unselected_values
+            torch.cuda.empty_cache()
 
         # if layer_idx == 31:
         #     counter = [0 for i in range(32)]
