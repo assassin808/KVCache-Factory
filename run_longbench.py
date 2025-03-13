082f9001189cdfe14d117a4bd4ca2657c4b8d7ae
assassin808 <assassin808@sjtu.edu.cn>
2025-03-13 15:22:26 +0800

    used for faster test

--- run_longbench.py
+++ run_longbench.py
@@ -9,10 +9,12 @@ from tqdm import tqdm
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-datasets = [ "narrativeqa","qasper", "multifieldqa_en",  "hotpotqa", "2wikimqa", "musique", \
-            "gov_report", "qmsum", "multi_news", "trec", "triviaqa", "samsum", \
-            "passage_count", "passage_retrieval_en", "lcc", "repobench-p"]
+datasets = [ "narrativeqa", "multifieldqa_en",  "hotpotqa", "2wikimqa", "musique", \
+              "passage_count", "passage_retrieval_en"]
 # datasets = ["trec", "triviaqa", "samsum", \
+#             "passage_count", "passage_retrieval_en", "lcc", "repobench-p"]
+# datasets = [ "narrativeqa",, "multifieldqa_en",  "hotpotqa", "2wikimqa", "musique", \
+#             "gov_report", "qmsum", "multi_news", "trec", "triviaqa", "samsum", \
 #             "passage_count", "passage_retrieval_en", "lcc", "repobench-p"]
 dataset2maxlen = {
     "narrativeqa": 128,
